adc490d71620334a35212a7f773374d6de85630e
JackDanielson <fedanielo@outlook.com>
2026-01-05 19:12:17 -0300

    Minor Fixes

--- cypress/e2e/tests/forms/practiceForm.cy.js
+++ cypress/e2e/tests/forms/practiceForm.cy.js
@@ -1,5 +1,5 @@
 const { should } = require('chai');
-const PracticeFormPage = require('../../../pages/forms/PracticeFormPage'); 
+const PracticeFormPage = require('../../../pages/forms/PracticeFormPage');
 
 describe('Testing Practice Form', () => {
 
@@ -7,42 +7,80 @@ describe('Testing Practice Form', () => {
         cy.visit('/automation-practice-form')
     });
 
-        it('Full Form filled - ', () => {
+    it('Full Form is filled with all data and modal shows everything correctly', () => {
         cy.fixture('dataPracticeForm.json').as('data')
 
         cy.get('@data').then((data) => {
             const user = data.fullUser
 
-            PracticeFormPage.typeText(PracticeFormPage.getTxtFirstName(), user.firstName)
-            PracticeFormPage.typeText(PracticeFormPage.getTxtLastName(), user.lastName)
-            PracticeFormPage.typeText(PracticeFormPage.getTxtUserEmail(), user.email)
-            PracticeFormPage.checkRadio(PracticeFormPage.getRadioGenderMale())
-            PracticeFormPage.typeText(PracticeFormPage.getTxtMobileNumber(), user.cellphone)
-            PracticeFormPage.selectDate(user.dob)
-            PracticeFormPage.typeSubjects(user.subjects)
-            if (user.hobbiesSports) PracticeFormPage.checkHobby(PracticeFormPage.getCheckHobbiesSports())
-            if (user.hobbiesReading) PracticeFormPage.checkHobby(PracticeFormPage.getCheckHobbiesReading())
-            if (user.hobbiesMusic) PracticeFormPage.checkHobby(PracticeFormPage.getCheckHobbiesMusic())
-            if (user.picture) PracticeFormPage.uploadFile(user.picture)
-            PracticeFormPage.typeText(PracticeFormPage.getTxtCurrentAddress(), user.currentAddress)
-            PracticeFormPage.selectState(user.state)
-            PracticeFormPage.selectCity(user.city)
+            if (user.firstName) PracticeFormPage.getTxtFirstName().type(user.firstName)
+            if (user.lastName) PracticeFormPage.getTxtLastName().type(user.lastName)
+            if (user.email) PracticeFormPage.getTxtUserEmail().type(user.email)
 
-            PracticeFormPage.submitForm()
+            PracticeFormPage.getRadioGenderMale().click({ force: true })
 
-            PracticeFormPage.getModalTitle().should('have.text', 'Thanks for submitting the form')
+            if (user.cellphone) PracticeFormPage.getTxtMobileNumber().type(user.cellphone)
+
+            PracticeFormPage.getCalDateOfBirth().click()
+            cy.get('.react-datepicker__month-select').select('April')
+            cy.get('.react-datepicker__year-select').select('1980')
+            cy.get('.react-datepicker__day--005:not(.react-datepicker__day--outside-month)').click()
+
+            PracticeFormPage.getTxtSubjects().type(user.subjects + '{enter}')
+
+
+            if (user.hobbiesSports) PracticeFormPage.getCheckHobbiesSports().click({ force: true })
+            if (user.hobbiesReading) PracticeFormPage.getCheckHobbiesReading().click({ force: true })
+            if (user.hobbiesMusic) PracticeFormPage.getCheckHobbiesMusic().click({ force: true })
+
+            if (user.picture) {
+                PracticeFormPage.getBtnChooseFile().selectFile(user.picture, { force: true })
+            }
+
+            if (user.currentAddress) PracticeFormPage.getTxtCurrentAddress().type(user.currentAddress)
+
+            PracticeFormPage.getBtnSubmit().click({ force: true })
+
+            PracticeFormPage.getModalTitle()
+                .should('have.text', 'Thanks for submitting the form')
 
             PracticeFormPage.getModalTableRows()
                 .eq(0).should('contain.text', 'Student Name').and('contain.text', `${user.firstName} ${user.lastName}`)
                 .parent().parent().eq(1).should('contain.text', 'Student Email').and('contain.text', user.email)
-                .parent().parent().eq(2).should('contain.text', 'Gender').and('contain.text', user.gender)
+                .parent().parent().eq(2).should('contain.text', 'Gender').and('contain.text', 'Male')
                 .parent().parent().eq(3).should('contain.text', 'Mobile').and('contain.text', user.cellphone)
-                .parent().parent().eq(4).should('contain.text', 'Date of Birth').and('contain.text', user.dob.replace(/\//g, ' '))
-                .parent().parent().eq(5).should('contain.text', 'Subjects').and('contain.text', user.subjects)
+                .parent().parent().eq(4).should('contain.text', 'Date of Birth').and('contain.text', '05 May,1980')
                 .parent().parent().eq(6).should('contain.text', 'Hobbies').and('contain.text', 'Sports').and('contain.text', 'Reading')
-                .parent().parent().eq(7).should('contain.text', 'Picture').and('contain.text', user.picture ? user.picture.split('/').pop() : '')
                 .parent().parent().eq(8).should('contain.text', 'Address').and('contain.text', user.currentAddress)
-                .parent().parent().eq(9).should('contain.text', 'State and City').and('contain.text', user.state).and('contain.text', user.city)
+                .parent().parent().eq(9).should('contain.text', 'State and City').and('contain.text', 'NCR').and('contain.text', 'Delhi')
         })
+    });
+
+    it('Minimum required fields filled - form submits successfully', () => {
+        cy.fixture('dataPracticeForm.json').as('data')
+
+        cy.get('@data').then((data) => {
+            const user = data.enoughDataUser
+
+            PracticeFormPage.typeText(PracticeFormPage.getTxtFirstName(), user.firstName)
+            PracticeFormPage.typeText(PracticeFormPage.getTxtLastName(), user.lastName)
+            PracticeFormPage.checkRadio(PracticeFormPage.getRadioGenderFemale())
+            PracticeFormPage.typeText(PracticeFormPage.getTxtMobileNumber(), user.cellphone)
+
+            PracticeFormPage.submitForm()
+            PracticeFormPage.getModalTitle().should('have.text', 'Thanks for submitting the form')
+            PracticeFormPage.getModalTableRows()
+                .eq(0).should('contain.text', 'Student Name').and('contain.text', `${user.firstName} ${user.lastName}`)
+                .parent().parent().eq(2).should('contain.text', 'Gender').and('contain.text', 'Female')
+                .parent().parent().eq(3).should('contain.text', 'Mobile').and('contain.text', user.cellphone)
+        })
+    });
+
+    it('Required fields validation - submit without data shows errors', () => {
+        PracticeFormPage.submitForm()
+        PracticeFormPage.getTxtFirstName().should('have.css', 'border-color', 'rgb(220, 53, 69)')
+        PracticeFormPage.getTxtLastName().should('have.css', 'border-color', 'rgb(220, 53, 69)')
+        PracticeFormPage.getTxtMobileNumber().should('have.css', 'border-color', 'rgb(220, 53, 69)')
+        PracticeFormPage.getModalTitle().should('not.exist')
     });
 });
--- cypress/pages/forms/practiceFormPage.js
+++ cypress/pages/forms/practiceFormPage.js
@@ -1,98 +1,70 @@
 class PracticeFormPage {
 
-    getTxtFirstName(){
+    // Solo getters — devuelven cy.get() directamente
+    getTxtFirstName() {
         return cy.get('#firstName')
     }
-
-    getTxtLastName(){
+    getTxtLastName() {
         return cy.get('#lastName')
     }
-
-    getTxtUserEmail(){
+    getTxtUserEmail() {
         return cy.get('#userEmail')
     }
 
-    getRadioGenderMale(){
+    getRadioGenderMale() {
         return cy.get('#gender-radio-1')
     }
-
-    getRadioGenderFemale(){
+    getRadioGenderFemale() {
         return cy.get('#gender-radio-2')
     }
-
-    getRadioGenderOther(){
+    getRadioGenderOther() {
         return cy.get('#gender-radio-3')
     }
 
-    getTxtMobileNumber(){
+    getTxtMobileNumber() {
         return cy.get('#userNumber')
     }
-
-    getCalDateOfBirth(){
-        return cy.get('#dateOfBirthInput') 
+    getCalDateOfBirth() {
+        return cy.get('#dateOfBirthInput')
     }
-
-    getTxtSubjects(){
+    getTxtSubjects() {
         return cy.get('#subjectsInput')
     }
 
-    getCheckHobbiesSports(){
+    getCheckHobbiesSports() {
         return cy.get('#hobbies-checkbox-1')
     }
-
-    getCheckHobbiesReading(){
+    getCheckHobbiesReading() {
         return cy.get('#hobbies-checkbox-2')
     }
-
-    getCheckHobbiesMusic(){
+    getCheckHobbiesMusic() {
         return cy.get('#hobbies-checkbox-3')
     }
 
-    getBtnChooseFile(){
+    getBtnChooseFile() {
         return cy.get('#uploadPicture')
     }
-
-    getTxtCurrentAddress(){
+    getTxtCurrentAddress() {
         return cy.get('#currentAddress')
     }
 
-    getComboState(){
+    getComboState() {
         return cy.get('#state')
     }
-
-    getComboCity(){
+    getComboCity() {
         return cy.get('#city')
     }
 
-    getBtnSubmit(){
+    getBtnSubmit() {
         return cy.get('#submit')
     }
 
-   getModalTitle(){
+    getModalTitle() {
         return cy.get('.modal-title')
     }
-
-    getModalTableRows(){
+    getModalTableRows() {
         return cy.get('.table-responsive table tbody tr')
     }
-
-    checkRadio(radio) {
-        radio.click({ force: true })
-    }
-
-    checkHobby(checkbox) {
-        checkbox.click({ force: true })
-    }
-
-    selectDate(dob) {
-        const [day, month, year] = dob.split('/')
-        this.getCalDateOfBirth().click()
-        cy.get('.react-datepicker__month-select').select(parseInt(month) - 1)
-        cy.get('.react-datepicker__year-select').select(year)
-        cy.get(`.react-datepicker__day--0${parseInt(day)}:not(.react-datepicker__day--outside-month)`).click()
-    }
-
-
 }
 
 module.exports = new PracticeFormPage()
